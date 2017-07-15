3986ac9057e266f0b155026d02a60f19baa56fe4
orysovan <olha.rysovana@gmail.com>
2017-07-15 16:05:10 +0300

    Add spaceship and bullet movement

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ window.onload = function () {
 
     window.loader = new Loader();
 
-    loader.preload(["img/cat.jpg"], function () {
+    loader.preload(["img/spaceship.png", "img/shoot.png"], function () {
+
+
+
 
         // creates renderer
         window.renderer = new Renderer();
@@ -13,54 +16,70 @@ window.onload = function () {
         // starts render loop
         renderer.animate(0);
 
-
         // create new Pixi Sprite with Pixi Texture from Loader
-        var cat = new PIXI.Sprite(loader.getTexture("img/cat.jpg"));
+        var spaceship = new PIXI.Sprite(loader.getTexture("img/spaceship.png"));
+        var bullet = new PIXI.Sprite(loader.getTexture("img/shoot.png"));
 
         //Set sprite scale
-        cat.scale.set(0.3);
-
-        //Set sprite position: x=200 and y=200
-        cat.position.set(200, 200);
-
+        spaceship.scale.set(0.245);
+        spaceship.position.set(400, 550);
         //Set sprite anchor: x=0.5 and y=0.5
-        cat.anchor.set(0.5, 0.5);
+        spaceship.anchor.set(0.5, 0.5);
+        spaceship.rotation += 3.1415926536;
 
-        //Create update function: moving cat with defined speed by selected angle
-        cat.update = function (dT) {
-            //Define moving object speed
-            var speed = 5 / 1000 * dT;
+        bullet.visible = false;
+        bullet.scale.set(0.245);
+        bullet.isShooting = false;
+        bullet.anchor.set(0.5, 0.5);
 
-            //Update sprite position
-            this.position.x += speed * Math.cos(this.rotation);
-            this.position.y += speed * Math.sin(this.rotation);
-        };
 
         //Create new instance of KeyboardManager
         var kMen = new KeyboardManager();
 
+        //Subcribe to "spaceDown" event
+        kMen.subscribe("spaceDown", function () {
+            if (!bullet.isShooting) {
+                bullet.visible = true;
+                bullet.position.x = spaceship.position.x;
+                bullet.position.y = spaceship.position.y;
+                bullet.isShooting = true;
+            }
+        });
+
+
         //Subcribe to "leftArrowDown" event
         kMen.subscribe("leftArrowDown", function () {
-            cat.rotation -= 0.1;
+            spaceship.position.x -= 15;
+            bullet.position.x -= 15;
         });
 
         //Subcribe to "rightArrowDown" event
         kMen.subscribe("rightArrowDown", function () {
-            cat.rotation += 0.1;
+            spaceship.position.x += 15;
+            bullet.position.x += 15;
         });
 
-        //Add sprite to main stage - sprite becomes visible
-        renderer.addToRenderLoop(cat);
 
-        //Add sprite to UpdateLoop - sprites update() function will be called with dT parameter
-        renderer.addToUpdateLoop(cat);
+        //Create update function: moving spaceship with defined speed by selected angle
+        spaceship.update = function (dT) {
 
-        /**
-         * Entry point of a game.
-         *
-         * Here you can add your game logic.
-         *
-         */
+            if (bullet.isShooting) {
+                // dT - millisecond, 5ms == 0.005 sec
+                //Define moving object speed
+                var pixelsPerDt = 200;
+                var speed = pixelsPerDt / 1000 * dT; // 20 / 5 = 4 pixels per one two hundred's of a second (5ms)
+
+                //Update sprite position
+                bullet.position.y -= speed;
+            }
+        };
+
+
+        renderer.addToRenderLoop(spaceship);
+        renderer.addToUpdateLoop(spaceship);
+        renderer.addToRenderLoop(bullet);
+        renderer.addToUpdateLoop(bullet);
+
     });
 };
 
